601daabbb5899c3dd4c9b173edef6f349a97786c
Igor <ig.vp@hotmail.com>
2022-07-13 08:24:41 -0300

    nada mais me deixa triste

--- methods.py
+++ methods.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-
+import cv2
 def TLinha(x,y,T, mediaT):
     return T[x][y] - mediaT
 
@@ -21,7 +21,10 @@ def R(x,y,I,T,mediaT):
             r_num+= T_Linha*I_Linha 
             r_dem_T+= (T_Linha)**2
             r_dem_I += (I_Linha)**2
-    r = r_num/(math.sqrt((r_dem_T*r_dem_I))) 
+    r_dem = (math.sqrt((r_dem_T*r_dem_I)))
+    if(r_dem ==0):
+        return 0
+    r = r_num/r_dem
     return r
 
 def template_matching(I,T):
@@ -29,26 +32,27 @@ def template_matching(I,T):
     W,H = I.shape
     mediaT = np.mean(T)#media de valores de T
     results = np.zeros((W-w+1,H-h+1))
+    print(results.shape)
     for x in range(len(results)):
         for y in range(len(results[0])):
             results[x][y] = R(x,y,I,T,mediaT)
+            print(x*len(results[0])+y)
     return results
 
 
 def main():
-    #I = np.array([[1.0,1.0,2.0,3.0],[2.0,3.0,4.0,5.0],
-    #[3.0,4.0,5.0,6.0],[3.0,4.0,5.0,6.0]])
-    #T = np.array([[4.0,5.0], [5.0, 6.1]])
-    I = np.random.randint(100,size=(4,4))
-    T = I[0:2,2:4]
-    result = template_matching(I,T)
-    for i in I:
-        print(i)
-    print("==================")
-    for i in T:
-        print(i)
-    print("==================")
-    for i in result:
-        print(i)
+    template = cv2.imread("./templates/cano_top.png")
+    template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    Image = cv2.imread("./templates/teste.png")
+    Image_gray = cv2.cvtColor(Image,cv2.COLOR_BGR2GRAY)
+    x,y,z = template.shape
+    I = np.random.randint(100,size=(5,6))
+    T = I[1:3,1:3]
+    #result = template_matching(Image_gray,template_gray)
+    result = cv2.matchTemplate(Image_gray, template_gray,cv2.TM_CCOEFF_NORMED)
+    Image = cv2.rectangle(Image,(181,37), (181+y,37+x),(0,0,255), 5)
+    cv2.imshow("Eye", Image)
+    cv2.waitKey(0)
+    print(np.unravel_index(np.argmax(result, axis=None), result.shape))
 if __name__ == "__main__":
     main()
