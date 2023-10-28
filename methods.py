fedb38f1434b0d16338b60ebb86ac62bc979061d
Igor <ig.vp@hotmail.com>
2023-10-28 16:02:18 -0300

    alterações para apresentacao

--- methods.py
+++ methods.py
@@ -2,15 +2,18 @@ from cv2 import CV_8U
 import numpy as np
 import math
 import cv2
-def TLinha(x,y,T, mediaT):
+
+
+def TLinha(x, y, T, mediaT):
     return T[x][y] - mediaT
 
-def ILinha(x,y,I,mediaI):
-    return I[x][y] - mediaI 
+
+def ILinha(x, y, I, mediaI):
+    return I[x][y] - mediaI
 
 
-def R(x,y,I,T,mediaT):
-    xt,yt = T.shape
+def R(x, y, I, T, mediaT):
+    xt, yt = T.shape
     r_num = 0
     r_dem_T = 0
     r_dem_I = 0
@@ -19,41 +22,47 @@ def R(x,y,I,T,mediaT):
     for xLinha in range(xt):
         for yLinha in range(yt):
             T_Linha = T[xLinha][yLinha] - mediaT
-            I_Linha = I[x+xLinha][y+yLinha] - mediaI 
-            r_num+= (T_Linha)*(I_Linha) 
-            r_dem_T+= (T_Linha)**2
+            I_Linha = I[x+xLinha][y+yLinha] - mediaI
+            r_num += (T_Linha)*(I_Linha)
+            r_dem_T += (T_Linha)**2
             r_dem_I += (I_Linha)**2
 
     r_dem = (math.sqrt((r_dem_T*r_dem_I)))
-    if(r_dem ==0):
+    if (r_dem == 0):
         return 0
     r = r_num/r_dem
     return r
 
-def template_matching(I,T):
-    w,h = T.shape
-    W,H = I.shape
-    mediaT = np.mean(T)#media de valores de T
-    results = np.zeros((W-w+1,H-h+1))
+
+def template_matching(I, T):
+    w, h = T.shape
+    W, H = I.shape
+    mediaT = np.mean(T)  # media de valores de T
+    results = np.zeros((W-w+1, H-h+1))
+    total = (W-w+1) * (H-h+1)
+    done = 0
     for x in range(len(results)):
         for y in range(len(results[0])):
-            results[x][y] = R(x,y,I,T,mediaT)
+            results[x][y] = R(x, y, I, T, mediaT)
+            done += 1
+            print(f"fiz:{done} de {total}")
     return results
 
 
 def main():
-    for i in range(10):
-        I = np.random.randint(255,size=(100,50))
-        I = np.uint8(I)
-        T = I[1:3,1:3]
-        result_1 = template_matching(I,T)
-        result_2 = cv2.matchTemplate(I, T,cv2.TM_CCOEFF_NORMED)
-        result_1 = np.unravel_index(np.argmax(result_1, axis=None), result_1.shape)
-        result_2 =  np.unravel_index(np.argmax(result_2, axis=None), result_2.shape)
-        print(result_1==result_2)
+    T = cv2.imread("./templates/btgtemplate.jpg")
+    T_gray = cv2.cvtColor(T, cv2.COLOR_BGR2GRAY)
+    I = cv2.imread("./templates/btgimage.jpg")
+    I_gray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+    y, x, _ = T.shape
+    result_1 = template_matching(I_gray, T_gray)
+    local_cord = np.where(result_1 >= 0.8)
+    for i in zip(*local_cord[::-1]):
+        cv2.rectangle(I, i, (i[0] + x, i[1] + y), (0, 0, 255), 5)
+
+    cv2.imshow("Eye", I)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
     main()
-
-    
